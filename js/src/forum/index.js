5dd0b0b8e4a08d55d73f2484b9fc18aad195795a
TPam <1807329232@qq.com>
2021-08-14 00:05:04 +0800

    add composer header

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -5,11 +5,18 @@ import classList from 'flarum/common/utils/classList';
 import DiscussionComposer from 'flarum/forum/components/DiscussionComposer';
 
 app.initializers.add('tpam/flarum-match', () => {
+  DiscussionComposer.prototype.addPoll = function () {
+    app.modal.show(CreatePollModal, {
+      poll: this.poll,
+      onsubmit: (poll) => (this.poll = poll),
+    });
+  };
+
     extend(DiscussionComposer.prototype, 'headerItems', function (items) {
       items.add(
-        'polls',
+        'match',
         <a className="DiscussionComposer-match" onclick={console.log('a')}>
-          <span className={classList('PollLabel', !this.poll && 'none')}>
+          <span className={classList('MatchLabel', !this.poll && 'none')}>
             {'发起拼车匹配'}
           </span>
         </a>,
